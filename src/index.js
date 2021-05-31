c02ece7e96735dfdb8be396670506c314d6d8b49
AxelQMC <84398496+AxelQMC@users.noreply.github.com>
2021-05-31 10:14:42 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const morgan = require("morgan");
+const exphbs = require("express-handlebars");
+const path = require("path");
+const { extname } = require("path");
+
+//initializations
+const app = express();
+
+//settings
+app.set("port",process.env.PORT || 4000);
+app.set("views", path.join(__dirname,"views"));
+app.engine(".hbs", exphbs ({
+    defaultLayout: "main",
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+    extname: ".hbs",
+    helpers: require ("./lib/handlebars")
+
+}));
+app.set("view engine", ".hbs");
+
+//middlewares
+app.use(morgan("dev"));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+//global variables
+app.use((req,res,next) => {
+
+    next();
+});
+
+//routes
+app.use(require("./routes/index.js"));
+//public
+
+//Starting the server
+app.listen(app.get("port"), () =>{
+    console.log("server on port",app.get ("port"));
+});
